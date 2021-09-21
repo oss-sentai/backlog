2a9058d26cf75cc89359be43412623f2bfbe67ee
kouheikishigami <kouhei19920522@gmail.com>
2021-09-22 02:26:56 +0900

    linterの設定など

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  roots: ["./src"],
-  testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
+  roots: ['./src'],
+  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  preset: "ts-jest",
+  preset: 'ts-jest',
 };
